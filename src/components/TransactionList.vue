21f7def4ae19146573bd7b52f10e8fdbacf83a04
BurnettMiao <searchfor1987@gmail.com>
2023-11-25 22:43:26 +0800

    project finish

---
+++ src/components/TransactionList.vue
@@ -0,0 +1,71 @@
+<template>
+  <h3>History</h3>
+  <ul id="list" class="list">
+    <li
+      v-for="transaction in transactions"
+      :key="transaction.id"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
+    >
+      {{ transaction.text }} <span>${{ transaction.amount }}</span
+      ><button @click="deleteTransaction(transaction.id)" class="delete-btn">
+        x
+      </button>
+    </li>
+    <!-- <li class="minus">
+      Cash <span>-$400</span><button class="delete-btn">x</button>
+    </li>
+    <li class="plus">
+      Paycheck <span>$800</span><button class="delete-btn">x</button>
+    </li> -->
+  </ul>
+</template>
+
+<!-- Composition API new -->
+<script setup>
+import { defineProps } from 'vue';
+
+const emit = defineEmits(['transactionDeleted']);
+
+const props = defineProps({
+  transactions: {
+    type: Array,
+    required: true,
+  },
+});
+
+const deleteTransaction = (id) => {
+  emit('transactionDeleted', id);
+};
+</script>
+
+<!-- Composition API -->
+<!-- <script>
+export default {
+  setup() {
+    const transactions = [
+      { id: 1, text: 'Flower', amount: -19.99 },
+      { id: 2, text: 'Salary', amount: 299.97 },
+      { id: 3, text: 'Book', amount: -10 },
+      { id: 4, text: 'Camera', amount: 150 },
+    ];
+
+    return { transactions };
+  },
+};
+</script> -->
+
+<!-- Options API -->
+<!-- <script>
+export default {
+  data() {
+    return {
+      transactions: [
+        { id: 1, text: 'Flower', amount: -19.99 },
+        { id: 2, text: 'Salary', amount: 299.97 },
+        { id: 3, text: 'Book', amount: -10 },
+        { id: 4, text: 'Camera', amount: 150 },
+      ],
+    };
+  },
+};
+</script> -->
---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './assets/style.css';
+import 'vue-toastification/dist/index.css';
+
+import { createApp } from 'vue';
+import Toast from 'vue-toastification';
+
+import App from './App.vue';
+
+const app = createApp(App);
+app.use(Toast);
+app.mount('#app');
